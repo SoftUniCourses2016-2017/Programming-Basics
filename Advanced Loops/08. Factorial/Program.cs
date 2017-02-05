c470eaabda786957807f278a080c5af541c9296e
Magdalena Kirova <magdalena.kkirova@gmail.com>
2017-02-06 00:37:42 +0200

    Advanced Loops

---
+++ Advanced Loops/08. Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int fact = 1;
+        do
+        {
+
+            fact = fact * n;
+            n--;
+
+        } while (n > 1);
+
+        Console.WriteLine(fact);
+    }
+}
+
