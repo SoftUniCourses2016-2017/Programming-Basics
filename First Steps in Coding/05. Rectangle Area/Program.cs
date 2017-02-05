8584b6508df702333892aafc860d34632f4a119a
Magdalena Kirova <magdalena.kkirova@gmail.com>
2017-02-05 22:38:51 +0200

    First steps in Coding

---
+++ First Steps in Coding/05. Rectangle Area/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactangleArea1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            var a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            var b = int.Parse(Console.ReadLine());
+            var area = a * b;
+            Console.Write("Sqare = ");
+            Console.WriteLine(area);
+        }
+    }
+}
